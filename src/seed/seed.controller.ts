8eaafcd5811898426be76183d97382585d81c94f
Deiber Josue Velandria Misse <deiberv@gmail.com>
2023-06-22 13:24:12 -0400

    fix: Eliminacion de control de acceso a seed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post } from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { ApiBearerAuth, ApiForbiddenResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Auth } from 'src/auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces';
 
@@ -18,10 +18,7 @@ export class SeedController {
     summary: "Para ejecutar esta accion es necesario iniciar session con un usuario de role ROLE_ADMIN"
   })
   @ApiOkResponse({ description: "Data cargada" })
-  @ApiUnauthorizedResponse({description: "Unauthorized" })
-  @ApiForbiddenResponse({description: "Forbidden" })
-  @ApiBearerAuth()
-  @Auth(ValidRoles.ADMIN)
+  @ApiInternalServerErrorResponse({description: "Error en el servidor"})
   runSeed() {
     return this.seedService.populateDB();
   }
